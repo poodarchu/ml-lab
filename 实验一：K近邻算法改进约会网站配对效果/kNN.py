02e13fa491294f6497b0ff7a7f37b04e62cefff6
poodarchu <poodarchu@gmail.com>
2016-06-11 18:06:36 +0800

    Finish Lab1:k近邻算法改善约会网站的配对效果

--- 实验一：K近邻算法改进约会网站配对效果/kNN.py
+++ 实验一：K近邻算法改进约会网站配对效果/kNN.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# python version: python 2.7.11
+
 '''
 kNN: k Nearest Neighbors
 Input: inX: vector to compare to existing dataset(1xN)
@@ -8,8 +10,10 @@ Input: inX: vector to compare to existing dataset(1xN)
        k: number of neighbors to use for comparision(should be an odd number)
 
 Output: the most popular class label
-@author: Poodar Chu
 '''
+
+# @author: Poodar Chu
+
 from numpy import *
 import operator  # 运算符模块
 from os import listdir
@@ -46,5 +50,89 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     # 返回发生频率最高的元素的标签
     return  sortedClassCount[0][0]
+
+# 输入: 文件名字字符串
+# 输出: 驯良样本矩阵和类标签向量
+def file2Matrix(fileName):
+    fr = open(fileName)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)  # 得到文件行数
+
+    # 创建以0填充的矩阵(实际上是一个二维数组)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+
+    # 循环读取每一行数据
+    index = 0
+    for line in arrayOLines:
+        # 去掉回车符
+        line = line.strip()
+        # 提取4个数据项
+        listFromLine = line.split('\t')
+        # 将前三项数据存入矩阵
+        returnMat[index, :] = listFromLine[0:3]
+        # 将第四项数据存入向量
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+# 归一化特征值
+def autoNorm(dataSet):
+    # 参数0使得函数可以从 列中 选取最小值,而不是选取当前行的最小值
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet/tile(ranges, (m, 1))
+    return  normDataSet, ranges, minVals
+
+# 测试算法的分类效果
+'''
+测试步骤:
+1. 读取数据文件中的样本数据到 特征矩阵 和 标签向量
+2. 对数据进行归一化,得到归一化的特征矩阵
+3. 执行kNN算法对测试数据进行测试,得到分类结果
+4. 与实际的分类结果进行对比,记录分类错误率
+5. 打印每个人的分类数据及错误率作为最终结果
+'''
+def datingClassTest():
+    # 设定测试数据的比例
+    hoRatio = 0.10
+
+    # 读取数据
+    datingDataMat, datingLabels = file2Matrix('data/datingTestSet2.txt')
+
+    # 归一化数据
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+
+    # 总行数
+    m = normMat.shape[0]
+
+    # 测试数据行数
+    numTestVecs = int(m*hoRatio)
+
+    # 初始化错误率
+    errorCount = 0.0
+
+    # 循环读取每行测试数据
+    for i in range(numTestVecs):
+        # 对该测试数据进行分类,该算法没有 "训练算法" 这一步,故直接进行测试。
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+
+        # 计算错误率
+        if(classifierResult != datingLabels[i]):
+            errorCount += 1.0
+
+    # 打印错误率
+    errorRatio = errorCount / float(numTestVecs)
+    print "the total error count is: %d. rate is: %f" % (errorCount, errorRatio)
+
+# 执行测试
+datingClassTest()
 
 
